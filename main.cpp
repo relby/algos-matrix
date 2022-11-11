5dd4dcd8ccf14fcd7fa59606b35822524b5be7d7
relby <kudinov.nikita@gmail.com>
2022-11-11 22:50:35 +0300

    All done but wihout sizeindex

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+//
+// main.cpp - Copyright (c) Гергель В.П. 07.05.2001
+//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (20.04.2015)
+//
+// Тестирование верхнетреугольной матрицы
+
+#include <iostream>
+#include "utmatrix.h"
+//---------------------------------------------------------------------------
+
+int main()
+{
+  TMatrix<int> a(5), b(5), c(5);
+  int i, j;
+
+  setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки представления треугольных матриц"
+    << endl;
+  for (i = 0; i < 5; i++)
+    for (j = i; j < 5; j++ )
+    {
+      a[i][j] =  i * 10 + j;
+      b[i][j] = (i * 10 + j) * 100;
+    }
+  
+  cout << "Matrix a = " << endl << a << endl;
+  cout << "Matrix b = " << endl << b << endl;
+
+  c = a + b;  // Doens't work because it doesn't have = operator overrided
+  cout << "Matrix a = " << endl << a << endl;
+  cout << "Matrix b = " << endl << b << endl;
+  cout << "Matrix c = a + b" << endl << c << endl;
+  cin >> c;
+  cout << "Matrix c = a + b" << endl << c << endl;
+}
+//---------------------------------------------------------------------------
